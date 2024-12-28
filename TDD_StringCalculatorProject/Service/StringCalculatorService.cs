e6175b0a00b169593f205e274cb31f498ab05950
etta.huang <etta.huang@ailabpte.co>
2024-12-29 02:07:19 +0800

    return sum test pass

--- TDD_StringCalculatorProject/Service/StringCalculatorService.cs
+++ TDD_StringCalculatorProject/Service/StringCalculatorService.cs
@@ -6,6 +6,17 @@ public class StringCalculatorService
 {
     public int Add(string intString)
     {
+        if (intString != null)
+        {
+            var split = intString.Split(',');
+            var sum = 0;
+            foreach (var s in split)
+            {
+                var number = Convert.ToInt32(s);
+                sum += number;
+            }
+            return sum;
+        }
         if(string.IsNullOrWhiteSpace(intString))
             return 0;
         
