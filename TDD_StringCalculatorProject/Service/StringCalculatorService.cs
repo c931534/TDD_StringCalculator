7042074d338950d5b9a7920e3aeb270a9c5cf491
etta.huang <etta.huang@ailabpte.co>
2024-12-29 03:14:39 +0800

    refactor

--- TDD_StringCalculatorProject/Service/StringCalculatorService.cs
+++ TDD_StringCalculatorProject/Service/StringCalculatorService.cs
@@ -11,23 +11,19 @@ public class StringCalculatorService
             var changeLineSum = 0;
 
             var changeLineSplit = numbers.Split("/n");
-            if (changeLineSplit.Count() > 1)
+            if (changeLineSplit.Length > 1)
             {
                 changeLineSum = int.Parse(changeLineSplit[0]);
             }
 
-            var commaList = new List<string>();
-            if (changeLineSplit[1].Any())
-                commaList = changeLineSplit[1].Split(",").ToList();
-            else
-                commaList = numbers.Split(",").ToList();
+            var commaList = changeLineSplit[1].Any()
+                ? changeLineSplit[1].Split(",").ToList()
+                : numbers.Split(",").ToList();
             return commaList
                 .Where(s => int.TryParse(s, out _)) // 過濾掉非數字項目
                 .Sum(s => Convert.ToInt32(s)) + changeLineSum;
         }
-        else
-        {
-            return 0;
-        }
+
+        return 0;
     }
 }
