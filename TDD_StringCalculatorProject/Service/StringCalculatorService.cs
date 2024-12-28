c88cc527e6ecb8b6f0ba45b4f77885af5f1e47ad
etta.huang <etta.huang@ailabpte.co>
2024-12-29 02:39:28 +0800

    return run with other split case pass

--- TDD_StringCalculatorProject/Service/StringCalculatorService.cs
+++ TDD_StringCalculatorProject/Service/StringCalculatorService.cs
@@ -1,14 +1,23 @@
 ﻿using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace TDD_StringCalculatorProject.Service;
 
 public class StringCalculatorService
 {
-    public int Add(string intString)
+    public int Add(string numbers)
     {
-        if (!string.IsNullOrWhiteSpace(intString))
+        if (!string.IsNullOrWhiteSpace(numbers))
         {
-            return intString.Split(',').Sum(s => Convert.ToInt32(s));
+
+            // 使用正則表達式提取正整數
+            var matches = Regex.Matches(numbers, @"\d+");
+            List<int> numberList = matches
+                .Select(m => int.Parse(m.Value))
+                .ToList();
+
+            // 計算正整數的總和
+            return numberList.Sum();
         }
         else
         {
