5379550d62e14ae01312dd6cf557157169f011fb
etta.huang <etta.huang@ailabpte.co>
2024-12-28 22:39:44 +0800

    add Service and method. default return 0

---
+++ TDD_StringCalculatorProject.Tests/Service/StringCalculatorServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TDD_StringCalculatorProject.Service;
+
+namespace TDD_StringCalculatorProject.Tests.Service;
+
+[TestFixture]
+[TestOf(typeof(StringCalculatorService))]
+public class StringCalculatorServiceTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
---
+++ TDD_StringCalculatorProject/Service/StringCalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+namespace TDD_StringCalculatorProject.Service;
+
+public class StringCalculatorService
+{
+    public int Add(string intString)
+    {
+        return 0;
+    }
+        
+}
