ef5013db4fa0d004e3a5953de7233111c9614cd9
etta.huang <etta.huang@ailabpte.co>
2024-12-29 20:52:20 +0800

    refactor

--- TDD_StringCalculatorProject/Service/StringCalculatorService.cs
+++ TDD_StringCalculatorProject/Service/StringCalculatorService.cs
@@ -14,34 +14,52 @@ public class StringCalculatorService
             {
                 if (!int.TryParse(splitArray[0], out splitNumber))
                 {
-                    splitChar = splitArray[0].Substring(splitArray[0].Length - 1);
-                    if (splitArray[0].Contains("["))
-                    {
-                        splitChar = splitArray[0].Substring(3, splitArray[0].Length - 4);
-                    }
+                    splitChar = SetSplitChar(splitArray);
                 }
             }
 
-            var splitList = MergeSplitList( splitArray, splitChar, splitNumber);
-            if (splitList.Any(number => int.TryParse(number, out splitNumber) && splitNumber < 0))
-            {
-                throw new ArgumentException($"Negatives not allowed: {splitNumber}");
-            }
+            var splitList = CombineSplitList(splitArray, splitChar, splitNumber);
 
-            return splitList.Where(s => int.TryParse(s, out int t) && t < 1000)
-                .Sum(s => Convert.ToInt32(s));
+            return splitList.Sum();
         }
 
         return 0;
     }
 
-    private List<string> MergeSplitList(string[] splitArray, string splitChar, int splitNumber)
+    private List<int> CombineSplitList(string[] splitArray, string splitChar, int splitNumber)
     {
-        var result = splitArray.Length > 1
+        var splitStringList = GetSplitStringList(splitArray, splitChar);
+        var intlist = new List<int>();
+
+        foreach (var number in splitStringList)
+        {
+            var convertNumber = 0;
+            if (int.TryParse(number, out convertNumber) && convertNumber < 0)
+                throw new ArgumentException($"Negatives not allowed: {convertNumber}");
+            intlist.Add(convertNumber);
+        }
+
+        intlist.Add(splitNumber);
+        return intlist.Where(x => x < 1000).ToList();
+    }
+
+    private string SetSplitChar(string[] splitArray)
+    {
+        string splitChar;
+        splitChar = splitArray[0].Substring(splitArray[0].Length - 1);
+        if (splitArray[0].Contains('['))
+        {
+            splitChar = splitArray[0].Substring(3, splitArray[0].Length - 4);
+        }
+
+        return splitChar;
+    }
+
+    private List<string> GetSplitStringList(string[] splitArray, string splitChar)
+    {
+        return splitArray.Length > 1
             ? SplitString(splitArray[1], splitChar)
             : SplitString(splitArray[0], splitChar);
-        result.Add(splitNumber.ToString());
-        return result;
     }
 
     private List<string> SplitString(string numbers, string splitChar)
