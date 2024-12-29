862846708174e7203ee261efab1a92f6975ef4dc
etta.huang <etta.huang@ailabpte.co>
2024-12-29 17:59:25 +0800

    refactor and fix outher test pass

--- TDD_StringCalculatorProject/Service/StringCalculatorService.cs
+++ TDD_StringCalculatorProject/Service/StringCalculatorService.cs
@@ -8,22 +8,35 @@ public class StringCalculatorService
     {
         if (!string.IsNullOrWhiteSpace(numbers))
         {
-            var splitChar = string.Empty;
-
-            var changeLineSplit = numbers.Split("\n");
-            if (changeLineSplit.Length > 1)
+            var splitChar = ",";
+            var splitNumber = 0;
+            if (IsNumberSplitByChangeLine(numbers, out string[] splitArray))
             {
-                splitChar = changeLineSplit[0].Substring(2);
+                if (!int.TryParse(splitArray[0], out splitNumber))
+                {
+                    splitChar = splitArray[0].Substring(splitArray[0].Length - 1);
+                }
             }
 
-            var commaList = changeLineSplit[1].Any()
-                ? changeLineSplit[1].Split(splitChar).ToList()
-                : numbers.Split(splitChar).ToList();
-            return commaList
-                .Where(s => int.TryParse(s, out _)) // 過濾掉非數字項目
-                .Sum(s => Convert.ToInt32(s));
+            return (splitArray.Length > 1
+                       ? SplitString(splitArray[1], splitChar)
+                       : SplitString(numbers, splitChar))
+                   .Where(s => int.TryParse(s, out _)) // 過濾掉非數字項目
+                   .Sum(s => Convert.ToInt32(s))
+                   + splitNumber;
         }
 
         return 0;
+    }
+
+    private List<string> SplitString(string numbers, string splitChar)
+    {
+        return numbers.Split(splitChar).ToList();
+    }
+
+    private bool IsNumberSplitByChangeLine(string numbers, out string[] splitResult)
+    {
+        splitResult = numbers.Split("\n");
+        return numbers.Split("\n").Length > 1;
     }
 }
